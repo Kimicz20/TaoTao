08640b0a4ae02ed48d38e2a27cc7cba0eefd5659
Fantasy <hz2z0421@163.com>
2017-06-28 10:31:23 +0800

    添加商品

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -2,14 +2,18 @@ package com.taotao.service.impl;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
-import com.taotao.pojo.EUIDataGridResult;
-import com.taotao.pojo.TbItem;
-import com.taotao.pojo.TbItemExample;
+import com.taotao.mapper.TbItemParamItemMapper;
+import com.taotao.mapper.TbItemParamMapper;
+import com.taotao.pojo.*;
+import com.taotao.pojo.TbItemDescExample.Criteria;
 import com.taotao.service.ItemService;
+import com.taotao.utils.IDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,6 +24,15 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     TbItemMapper itemMapper;
+
+    @Autowired
+    TbItemParamMapper tbItemParamMapper;
+
+    @Autowired
+    TbItemDescMapper tbItemDescMapper;
+
+    @Autowired
+    TbItemParamItemMapper tbItemParamItemMapper;
 
     @Override
     public TbItem ItemQueryById(long id) {
@@ -42,5 +55,84 @@ public class ItemServiceImpl implements ItemService {
         result.setRows(list);
 
         return result;
+    }
+
+    @Override
+    public TaotaoResult insertTbItem(TbItem tbItem, String desc, String itemParams) throws Exception {
+        //补全信息
+        Long id = IDUtils.genItemId();
+        tbItem.setId(id);
+        tbItem.setStatus((byte) 1);
+        tbItem.setCreated(new Date());
+        tbItem.setUpdated(new Date());
+        itemMapper.insert(tbItem);
+
+        //描述信息
+        TaotaoResult result = insertTbItemDesc(id,desc);
+
+        if (result.getStatus() != 200){
+            throw new Exception();
+        }
+
+        //添加商品规格参数
+        result = insertTbItemParamItem(id,itemParams);
+        if (result.getStatus() != 200){
+            throw new Exception();
+        }
+        return TaotaoResult.ok();
+    }
+
+    /**
+     * 添加商品规格参数
+     * @param id
+     * @param itemParams
+     * @return
+     */
+    public TaotaoResult insertTbItemParamItem(long id,String itemParams) {
+        TbItemParamItem item =  new TbItemParamItem();
+        item.setItemId(id);
+        item.setParamData(itemParams);
+        item.setCreated(new Date());
+        item.setUpdated(new Date());
+        tbItemParamItemMapper.insert(item);
+        return TaotaoResult.ok();
+    }
+
+    /**
+     * 添加商品描述信息
+     * @param id
+     * @param desc
+     * @return
+     */
+    public TaotaoResult insertTbItemDesc(long id,String desc) {
+        TbItemDesc itemDesc =  new TbItemDesc();
+        itemDesc.setItemId(id);
+        itemDesc.setItemDesc(desc);
+        itemDesc.setCreated(new Date());
+        itemDesc.setUpdated(new Date());
+        tbItemDescMapper.insert(itemDesc);
+        return TaotaoResult.ok();
+    }
+
+    @Override
+    public TaotaoResult queryParam(long id) {
+        TbItemParamExample tbItemParamExample = new TbItemParamExample();
+        TbItemParamExample.Criteria criteria =  tbItemParamExample.createCriteria();
+        criteria.andItemCatIdEqualTo(id);
+        List<TbItemParam> list = tbItemParamMapper.selectByExampleWithBLOBs(tbItemParamExample);
+        //查询到结果
+        if(list != null && list.size() >0){
+            return TaotaoResult.ok(list.get(0));
+        }
+        return TaotaoResult.ok();
+    }
+
+    @Override
+    public TaotaoResult insertParam(TbItemParam tbItemParam) {
+
+        tbItemParam.setCreated(new Date());
+        tbItemParam.setUpdated(new Date());
+        tbItemParamMapper.insert(tbItemParam);
+        return TaotaoResult.ok();
     }
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,15 +1,14 @@
 package com.taotao.controller;
 
-import com.taotao.pojo.EUIDataGridResult;
-import com.taotao.pojo.TbItem;
+import com.taotao.pojo.*;
+import com.taotao.service.ItemCatService;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by geek on 2017/6/11.
@@ -21,6 +20,9 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @Autowired
+    private ItemCatService itemCatService;
+
     @RequestMapping("/{id}")
     @ResponseBody
     TbItem ItemQuery(@PathVariable Long id){
@@ -29,8 +31,36 @@ public class ItemController {
 
     @RequestMapping("/list")
     @ResponseBody
-    EUIDataGridResult showItemList(@RequestParam(defaultValue="1")Integer page,
+    EUIDataGridResult getItemList(@RequestParam(defaultValue="1")Integer page,
                                    @RequestParam(defaultValue="30")Integer rows){
         return itemService.getPageList(page,rows);
+    }
+
+    @RequestMapping("/cat/list")
+    @ResponseBody
+    List<EUITreeNode> getCatList(@RequestParam(value = "id",
+                            defaultValue="0")Long id){
+        return itemCatService.getCatList(id);
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @ResponseBody
+    TaotaoResult addTbItem(TbItem tbItem, String desc, String itemParams) throws Exception {
+        return itemService.insertTbItem(tbItem,desc,itemParams);
+    }
+
+    @RequestMapping("/param/query/itemcatid/{itemcatid}")
+    @ResponseBody
+    TaotaoResult queryItemParam(@PathVariable Long itemcatid){
+        return itemService.queryParam(itemcatid);
+    }
+
+    @RequestMapping(value = "/param/save/{cid}",method = RequestMethod.POST)
+    @ResponseBody
+    TaotaoResult addParam(@PathVariable Long cid,@RequestParam("paramData") String paramData){
+        TbItemParam tbItemParam = new TbItemParam();
+        tbItemParam.setItemCatId(cid);
+        tbItemParam.setParamData(paramData);
+        return itemService.insertParam(tbItemParam);
     }
 }
